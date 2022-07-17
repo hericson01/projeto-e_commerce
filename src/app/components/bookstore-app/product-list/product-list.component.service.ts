b4d8ade95302ad5bace8054d59d0d08862d57da4
hericson01 <hericson00@gmail.com>
2022-07-17 16:49:22 -0300

    Iniciando servive e contructor

--- src/app/components/bookstore-app/product-list/product-list.component.service.ts
+++ src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -2,10 +2,17 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Book } from "./model/Book";
 
+export const books: Book[] = [
+    { id: '1', name: 'Book1' , price: 10, quantity: 1, category: "Acao", img:"img1"},
+    { id: '2', name: 'Book2' , price: 10, quantity: 1, category: "Acao", img:"img2"},
+    { id: '3', name: 'Book3' , price: 10, quantity: 1, category: "Acao", img:"img3"},
+
+  ];
+
 @Injectable()
 
 export class BookService{
-    private url = 'http://localhost:4200/';
+    private url = 'https://localhost:44382/api/bookstore';
     httpOptions = {
         Headers: new HttpHeaders ( { 'content-type': 'aplication/json'})
     }
